054e480f03ed96ff330b65e585e98dae5dd17703
SibteNajam <136195210+SibteNajam@users.noreply.github.com>
2025-03-08 14:54:22 +0500

    pretier

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,21 +1,16 @@
-import { Button } from "@mui/material";
+// import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
   const navigate = useNavigate();
   return (
     <div>
-      <h1 className="text-xl text-yellow-500 font-semibold">
+      <h1 className="text-xl text-yellow-500 font-semibold text-center">
         The best pizza.
         <br />
         Straight out of the oven, straight to you.
       </h1>
-      <Button
-        onClick={() => navigate("/menu")}
-        className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue"
-      >
-        Click to Menu
-      </Button>
+      {/* <Button onClick={() => navigate("/menu")}>Click to Menu</Button> */}
     </div>
   );
 }
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
