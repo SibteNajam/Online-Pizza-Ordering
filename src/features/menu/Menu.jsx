636ffb0cf430f2cd829f6b279a345151a2d7c40f
SibteNajam <136195210+SibteNajam@users.noreply.github.com>
2025-04-29 15:56:18 +0500

    menu menu item modified

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,16 +1,43 @@
+// import { useLoaderData } from "react-router-dom";
+// import { getMenu } from "../../services/apiRestaurant";
+// import MenuItem from "./MenuItem";
+// function Menu() {
+//   const menu = useLoaderData(); // this hook automatically know that data from menuLoader
+//   console.log("checkmenuformT", { menu });
+//   return (
+//     // this divide class placed in parent item that add some line between child elements
+//     <ul className="divide-y divide-stone-200 px-2">
+//       {menu.map((item) => (
+//         <MenuItem key={item.id} pizza={item} />
+//       ))}
+//     </ul>
+//   );
+// }
+
+// export async function loader() {
+//   const menu = await getMenu();
+//   return menu;
+// }
+// export default Menu;
 import { useLoaderData } from "react-router-dom";
 import { getMenu } from "../../services/apiRestaurant";
 import MenuItem from "./MenuItem";
+
 function Menu() {
-  const menu = useLoaderData(); // this hook automatically know that data from menuLoader
-  console.log("checkmenuformT", { menu });
+  const menu = useLoaderData();
   return (
-    // this divide class placed in parent item that add some line between child elements
-    <ul className="divide-y divide-stone-200 px-2">
-      {menu.map((item) => (
-        <MenuItem key={item.id} pizza={item} />
-      ))}
-    </ul>
+    <div className="min-h-screen p-4 pizza-bg relative">
+      <div className="crystal-container max-w-4xl mx-auto">
+        <h2 className="text-3xl md:text-4xl text-gray-900 font-extrabold text-center mb-8 animate-fade-in">
+          Our Delicious Menu
+        </h2>
+        <ul className="space-y-4">
+          {menu.map((item) => (
+            <MenuItem key={item.id} pizza={item} className="animate-slide-up" />
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
 
@@ -18,4 +45,5 @@ export async function loader() {
   const menu = await getMenu();
   return menu;
 }
+
 export default Menu;
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,11 +1,65 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
+// /* eslint-disable no-unused-vars */
+// /* eslint-disable react/prop-types */
+// import { formatCurrency } from "../../utilities/helpers";
+// import Button from "../../ui/Button";
+// import { useDispatch, useSelector } from "react-redux";
+// import { addItem, getCart } from "../cart/cartSlice";
+// import UpdateItemQuantity from "../cart/UPdateItemQuantity";
+// function MenuItem({ pizza }) {
+//   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+//   const cart = useSelector(getCart);
+//   const currentItem = cart.find((item) => item.pizzaId === id);
+//   const dispatch = useDispatch();
+
+//   function handleAddToCart() {
+//     const newItem = {
+//       pizzaId: id,
+//       name,
+//       quantity: 1,
+//       unitPrice,
+//       totalPrice: unitPrice * 1,
+//     };
+//     dispatch(addItem(newItem));
+//   };
+
+//   return (
+//     <li className="flex gap-7 py-2">
+//       <img src={imageUrl} alt={name} className={`h-24 ${soldOut ? 'opacity-90 grayscale-80' : ''}`} />
+//       <div className="flex flex-col grow pt-0.5">
+//         <p className="font-medium ">{name}</p>
+//         {/* <p className="text-sm italic text-stone-500 capitalize">{ingredients.join(", ")}</p> */}
+//         <div className="flex flex-wrap gap-1 text-sm text-gray-600">
+//           {ingredients.map((ing, i) => (
+//             <span key={i} className="bg-stone-100 px-1.5 py-0.5 rounded">
+//               {ing}
+//             </span>
+//           ))}
+//         </div>
+
+//         <div className="mt-auto flex items-center justify-between">
+//           {!soldOut ? <p className="text-sm">{formatCurrency(unitPrice)}</p> : <p className="text-sm text-yellow-500 uppercase font-medium">Sold out</p>}
+//           <div className="flex items-center space-x-3">
+//             {currentItem && (
+//               <UpdateItemQuantity pizza={currentItem} />
+//             )}
+//             {!soldOut && <Button type='small' onClick={handleAddToCart} disabled={currentItem} className="transition-all duration-300 ease-in-out hover:scale-105 hover:brightness-110"
+//             > Add To cart </Button>
+//             }
+//           </div>
+//         </div>
+//       </div>
+//     </li>
+//   );
+// }
+
+// export default MenuItem;
 import { formatCurrency } from "../../utilities/helpers";
 import Button from "../../ui/Button";
 import { useDispatch, useSelector } from "react-redux";
 import { addItem, getCart } from "../cart/cartSlice";
 import UpdateItemQuantity from "../cart/UPdateItemQuantity";
-function MenuItem({ pizza }) {
+
+function MenuItem({ pizza, className }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
   const cart = useSelector(getCart);
   const currentItem = cart.find((item) => item.pizzaId === id);
@@ -20,28 +74,42 @@ function MenuItem({ pizza }) {
       totalPrice: unitPrice * 1,
     };
     dispatch(addItem(newItem));
-  };
+  }
 
   return (
-    <li className="flex gap-7 py-2">
-      <img src={imageUrl} alt={name} className={`h-24 ${soldOut ? 'opacity-90 grayscale-80' : ''}`} />
-      <div className="flex flex-col grow pt-0.5">
-        <p className="font-medium ">{name}</p>
-        {/* <p className="text-sm italic text-stone-500 capitalize">{ingredients.join(", ")}</p> */}
-        <div className="flex flex-wrap gap-1 text-sm text-gray-600">
+    <li
+      className={`flex gap-4 py-4 px-4 bg-white bg-opacity-20 rounded-xl shadow-lg hover:shadow-xl transition-all ${className} ${soldOut ? "opacity-80 grayscale" : ""
+        }`}
+    >
+      <img
+        src={imageUrl}
+        alt={name}
+        className="h-24 w-24 object-cover rounded-lg"
+      />
+      <div className="flex flex-col flex-grow">
+        <p className="text-lg font-semibold text-gray-900">{name}</p>
+        <div className="flex flex-wrap gap-2 text-xs text-gray-700 my-2">
           {ingredients.map((ing, i) => (
-            <span key={i} className="bg-stone-100 px-1.5 py-0.5 rounded">
+            <span
+              key={i}
+              className="bg-yellow-100 px-2 py-1 rounded-full"
+            >
               {ing}
             </span>
           ))}
         </div>
-
         <div className="mt-auto flex items-center justify-between">
-          {!soldOut ? <p className="text-sm">{formatCurrency(unitPrice)}</p> : <p className="text-sm text-yellow-500 uppercase font-medium">Sold out</p>}
+          {!soldOut ? (
+            <p className="text-sm text-gray-800">
+              {formatCurrency(unitPrice)}
+            </p>
+          ) : (
+            <p className="text-sm text-yellow-500 font-semibold uppercase">
+              Sold out
+            </p>
+          )}
           <div className="flex items-center space-x-3">
-            {currentItem && (
-              <UpdateItemQuantity pizza={currentItem} />
-            )}
+            {currentItem && <UpdateItemQuantity pizza={currentItem} />}
             {!soldOut && <Button type='small' onClick={handleAddToCart} disabled={currentItem} className="transition-all duration-300 ease-in-out hover:scale-105 hover:brightness-110"
             > Add To cart </Button>
             }
