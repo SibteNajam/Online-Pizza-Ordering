85ca6c1a09f67dee555f0bb45600a35956cea509
SibteNajam <136195210+SibteNajam@users.noreply.github.com>
2025-05-04 21:01:06 +0500

    added-framer motion

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,34 +1,3 @@
-// import { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-
-// function SearchOrder() {
-//   const [query, setQuery] = useState("");
-//   const navigate = useNavigate();
-
-//   function handleSubmit(e) {
-//     e.preventDefault();
-//     if (!query) return;
-//     navigate(`/order/${query}`);
-//     setQuery(""); // Clear the input after navigation
-//   }
-
-//   return (
-//     <form onSubmit={handleSubmit}>
-//     <input
-
-//   className="rounded-full bg-yellow-100 px-4 py-2 text-sm placeholder:text-stone-400 focus:w-72 sm:focus:w-64 border border-transparent focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 transition-all duration-300"
-//   placeholder="Search Order"
-//   value={query}
-//   onChange={(e) => { 
-//     setQuery(e.target.value);
-//   }} 
-// />
-
-//     </form>
-//   );
-// }
-
-// export default SearchOrder;
 import { useState } from "react";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
